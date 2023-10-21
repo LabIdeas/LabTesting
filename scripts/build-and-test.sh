3932998cecdbec83acfd4952f1a3f2b34c4a80a7
jaminiSS <139842294+jaminiSS@users.noreply.github.com>
2023-10-21 21:38:17 +0530

    Update build-and-test.sh

--- scripts/build-and-test.sh
+++ scripts/build-and-test.sh
@@ -1,58 +1,8 @@
-# #!/bin/bash
-
-# # Temporary directory to clone the repository
-# TEMP_DIR="$(mktemp -d)"
-
-# # Define your project's Git repository URL
-# PROJECT_GIT_URL="https://github.com/yourusername/your-selenium-project.git"
-
-# # Clone the project from Git to the temporary directory
-# git clone "$PROJECT_GIT_URL" "$TEMP_DIR"
-
-# # Navigate to the project directory
-# cd "$TEMP_DIR"
-
-# # Fetch project dependencies (e.g., using SBT)
-# sbt update
-
-# # Compile the project
-# sbt compile
-
-# # Run Selenium tests
-# sbt test
-
-# # Clean up the temporary directory
-# rm -rf "$TEMP_DIR"
-
-
-
-
 #!/bin/bash
 
-# Define the URL of the JavaScript file in your Git repository
-JAVASCRIPT_FILE_URL="https://raw.githubusercontent.com/LabIdeas/LabTesting/main/src/test/scala/helloworld.javascript"
+# Specify the GitHub repository URL
+REPO_URL="https://api.github.com/repos/LabIdeas/LabTesting/contents/hello.js"  # Use the API endpoint for the file
 
-# Check if Node.js is installed
-if ! command -v node &> /dev/null; then
-  echo "Node.js is not installed. Installing it now..."
-  
-  # Install Node.js (adjust the version as needed)
-  curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-  sudo apt-get install -y nodejs
-  
-  # Verify the installation
-  if ! command -v node &> /dev/null; then
-    echo "Failed to install Node.js. Exiting..."
-    exit 1
-  fi
-fi
-
-# Download the JavaScript file from the Git repository
-curl -o helloworld.javascript "$JAVASCRIPT_FILE_URL"
-
-# Run the downloaded JavaScript file with Node.js
-node helloworld.javascript
-
-# Clean up the downloaded file
-rm -f helloworld.javascript
-
+# Execute the JavaScript script from the GitHub repository directly
+curl -sLJO "$REPO_URL"
+node hello.js
