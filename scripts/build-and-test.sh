1548654fc8bbdbcc89f24dd78f42f8749019ee2c
jaminiSS <139842294+jaminiSS@users.noreply.github.com>
2023-10-21 21:17:00 +0530

    Update build-and-test.sh

--- scripts/build-and-test.sh
+++ scripts/build-and-test.sh
@@ -1,25 +1,58 @@
+# #!/bin/bash
+
+# # Temporary directory to clone the repository
+# TEMP_DIR="$(mktemp -d)"
+
+# # Define your project's Git repository URL
+# PROJECT_GIT_URL="https://github.com/yourusername/your-selenium-project.git"
+
+# # Clone the project from Git to the temporary directory
+# git clone "$PROJECT_GIT_URL" "$TEMP_DIR"
+
+# # Navigate to the project directory
+# cd "$TEMP_DIR"
+
+# # Fetch project dependencies (e.g., using SBT)
+# sbt update
+
+# # Compile the project
+# sbt compile
+
+# # Run Selenium tests
+# sbt test
+
+# # Clean up the temporary directory
+# rm -rf "$TEMP_DIR"
+
+
+
+
 #!/bin/bash
 
-# Temporary directory to clone the repository
-TEMP_DIR="$(mktemp -d)"
+# Define the URL of the JavaScript file in your Git repository
+JAVASCRIPT_FILE_URL="https://raw.githubusercontent.com/LabIdeas/LabTesting/main/path/to/your/helloworld.javascript"
 
-# Define your project's Git repository URL
-PROJECT_GIT_URL="https://github.com/yourusername/your-selenium-project.git"
+# Check if Node.js is installed
+if ! command -v node &> /dev/null; then
+  echo "Node.js is not installed. Installing it now..."
+  
+  # Install Node.js (adjust the version as needed)
+  curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+  sudo apt-get install -y nodejs
+  
+  # Verify the installation
+  if ! command -v node &> /dev/null; then
+    echo "Failed to install Node.js. Exiting..."
+    exit 1
+  fi
+fi
 
-# Clone the project from Git to the temporary directory
-git clone "$PROJECT_GIT_URL" "$TEMP_DIR"
+# Download the JavaScript file from the Git repository
+curl -o helloworld.javascript "$JAVASCRIPT_FILE_URL"
 
-# Navigate to the project directory
-cd "$TEMP_DIR"
+# Run the downloaded JavaScript file with Node.js
+node helloworld.javascript
 
-# Fetch project dependencies (e.g., using SBT)
-sbt update
+# Clean up the downloaded file
+rm -f helloworld.javascript
 
-# Compile the project
-sbt compile
-
-# Run Selenium tests
-sbt test
-
-# Clean up the temporary directory
-rm -rf "$TEMP_DIR"
