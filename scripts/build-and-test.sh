8b98d3157ac314ea10ffaaddc612517e8b7b3385
jaminiSS <139842294+jaminiSS@users.noreply.github.com>
2023-10-21 21:52:02 +0530

    Update build-and-test.sh

--- scripts/build-and-test.sh
+++ scripts/build-and-test.sh
@@ -6,3 +6,5 @@ REPO_URL="https://api.github.com/repos/LabIdeas/LabTesting/contents/hello.js"  #
 # Execute the JavaScript script from the GitHub repository directly
 curl -sLJO "$REPO_URL"
 node hello.js
+
+console.log('hello world')
