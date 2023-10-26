d780e91196de2f74a7302b96b9c9c308ae5355bd
jaminiSS <139842294+jaminiSS@users.noreply.github.com>
2023-10-26 20:51:35 +0530

    Update build-and-test.sh

--- scripts/build-and-test.sh
+++ scripts/build-and-test.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-
-# Specify the GitHub repository URL
-REPO_URL="https://api.github.com/repos/LabIdeas/LabTesting/contents/hello.js"  # Use the API endpoint for the file
-
-# Execute the JavaScript script from the GitHub repository directly
-curl -sLJO "$REPO_URL"
-node hello.js
-
-console.log('hello world')
+/bin/bash -c "npm install"
+/bin/bash -c "node hello.js"
+/bin/bash -c "npm i axios"
