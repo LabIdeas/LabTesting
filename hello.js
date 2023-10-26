85d13c0d89e04a290fa4cf44b73a3ada8a33aca6
jaminiSS <139842294+jaminiSS@users.noreply.github.com>
2023-10-26 20:42:39 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,2 +1,23 @@
 // hello.js
-console.log("Hello, World!");
+// console.log("Hello, World!");
+
+
+
+const axios = require('axios');
+
+// Replace with the API endpoint URL
+const apiUrl = 'https://hub.worldpop.org/rest/data/pop';
+
+// Example test for making a GET request
+test('GET request to API endpoint', async () => {
+  try {
+    const response = await axios.get(apiUrl);
+    
+    // Replace with your test assertions
+    expect(response.status).toBe(200);
+    // Add more assertions as needed
+  } catch (error) {
+    // Handle any errors or failed assertions
+    throw error;
+  }
+});
