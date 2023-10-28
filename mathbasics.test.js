78c68e66c99ede54fa792611d1f9c6b3fc7a1023
Shashiniw <58620099+shashiniw@users.noreply.github.com>
2023-10-28 11:39:15 +0530

    Update mathbasics.test.js

--- mathbasics.test.js
+++ mathbasics.test.js
@@ -1,13 +1,15 @@
-const mathbasics = require('./mathbasics');
+const {add,sub} = require('./mathbasics');
 //const  assert = require('chai').assert;
 import {assert} from 'chai';
 
 describe('add 1+2 to equal 3', () => {
     it('should add two numbers', function () {
-      assert.equal(mathbasics.add(1,2), 3);
+      assert.equal(add(1,2), 3);
+        //assert.equal(mathbasics.add(1,2), 3);
     });
      it('should sub two numbers', function () {
-      assert.equal(mathbasics.sub(1,2), -1);
+      assert.equal(sub(1,2), -1);
+         //assert.equal(mathbasics.sub(1,2), -1);
     });
 });
 
