66c5320ae3da87bc7d43cb9f67284be4d32227a2
Shashiniw <58620099+shashiniw@users.noreply.github.com>
2023-10-28 09:22:15 +0530

    Update and rename api.test.js to mathbasics.test.js

---
+++ mathbasics.test.js
@@ -0,0 +1,10 @@
+const {add,sub} = require('./mathbasics');
+
+test('add 1+2 to equal 3', async () => {
+    expect(add(1,2).toBe(3);
+});
+
+test('sub 1-2 to equal -1', async () => {
+    expect(add(1,2).toBe(-1);
+});
+
