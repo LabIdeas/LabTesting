7b11aab16e24ce8b245f90cbbee77aca27d5ad03
Shashiniw <58620099+shashiniw@users.noreply.github.com>
2023-11-01 09:30:27 +0530

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -2,24 +2,44 @@ var Mocha = require('mocha');
 
 var mocha = new Mocha({});
 
-mocha.addFile('./mathbasics.test.js')
+var output_json={}
+var inner_json={}
 
+function getTime() {
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+    return dateTime;
+}
+
+mocha.addFile('./mathbasics.test.js')
 mocha.run()
     .on('test', function(test) {
-        console.log('Test started: '+test.title);
+        //console.log('Test started: '+test.title);
+        inner_json['Test started']=test.title
     })
     .on('test end', function(test) {
-        console.log('Test done: '+test.title);
+        //console.log('Test done: '+test.title);
+        inner_json['Test ended']=test.title
     })
     .on('pass', function(test) {
-        console.log('Test passed');
-        console.log(test);
+        // console.log('Test passed');
+        // console.log(test);
+        inner_json['Test passed']=test
     })
     .on('fail', function(test, err) {
-        console.log('Test fail');
-        console.log(test);
-        console.log(err);
+        inner_json['Test fail']=test
+        inner_json['Test fail Error']=err
+        // console.log('Test fail');
+        // console.log(test);
+        // console.log(err);
     })
     .on('end', function() {
-        console.log('All done');
+        // console.log('All done');
+        inner_json['All done']=getTime()
     });
+
+outer_json[getTime()]=inner_json
+console.log(outer_json)
+
