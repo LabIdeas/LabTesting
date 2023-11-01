b1f5e220af5a8a252f9c107f7c99aa713568f8fe
Shashiniw <58620099+shashiniw@users.noreply.github.com>
2023-11-01 13:03:29 +0530

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -27,11 +27,11 @@ mocha.run()
     .on('pass', function(test) {
         // console.log('Test passed');
         // console.log(test);
-        inner_json["Test passed-"+counter.toString()]=test
+        inner_json["Test passed-"+counter.toString()]=String(test)
         counter+=1
     })
     .on('fail', function(test, err) {
-        inner_json["Test fail-"+counter.toString()]=test
+        inner_json["Test fail-"+counter.toString()]=String(test)
         inner_json["Test fail Error"]=err
         counter+=1
         // console.log('Test fail');
